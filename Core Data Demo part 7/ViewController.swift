1326ad2599efeeea0c950cf2c3826b537d06cc15
trungld3 <k21tpm4@gmail.com>
2020-06-08 17:08:12 +0700

    Initial Commit

---
+++ Core Data Demo part 7/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Core Data Demo part 7
+//
+//  Created by TrungLD on 6/8/20.
+//  Copyright © 2020 TrungLD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
